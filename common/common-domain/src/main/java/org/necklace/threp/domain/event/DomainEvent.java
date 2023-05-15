c5bc627221395fb09f9c87598f2a278526c1d911
necklace17 <niklas.scholz@outlook.com>
2023-05-15 21:40:52 +0800

    finished common module

---
+++ common/common-domain/src/main/java/org/necklace/threp/domain/event/DomainEvent.java
@@ -0,0 +1,6 @@
+package org.necklace.threp.domain.event;
+
+public interface DomainEvent<T> {
+
+
+}
---
+++ common/common-domain/src/main/java/org/necklace/threp/domain/valueobject/ProductId.java
@@ -0,0 +1,10 @@
+package org.necklace.threp.domain.valueobject;
+
+import java.util.UUID;
+
+public class ProductId extends BaseId<UUID> {
+
+  protected ProductId(UUID value) {
+    super(value);
+  }
+}
