337533d96f92ed2189bd1b8ab080963230658385
necklace17 <niklas.scholz@outlook.com>
2023-05-15 21:37:44 +0800

    created common objects

--- common/common-domain/src/main/java/org/necklace/threp/domain/entity/AggregateRoot.java
+++ common/common-domain/src/main/java/org/necklace/threp/domain/entity/AggregateRoot.java
@@ -1,4 +1,4 @@
-package org.necklace.domain.entity;
+package org.necklace.threp.domain.entity;
 
 public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
 
--- common/common-domain/src/main/java/org/necklace/threp/domain/entity/BaseEntity.java
+++ common/common-domain/src/main/java/org/necklace/threp/domain/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package org.necklace.domain.entity;
+package org.necklace.threp.domain.entity;
 
 import java.util.Objects;
 
